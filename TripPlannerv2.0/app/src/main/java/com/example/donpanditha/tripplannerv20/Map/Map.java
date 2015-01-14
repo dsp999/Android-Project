02d94249804e04f15c3502dac52829a441fc17b2
dsp999 <dspanditha@yahoo.com>
2015-01-14 11:52:48 +1100

    remove project

--- TripPlannerv2.0/app/src/main/java/com/example/donpanditha/tripplannerv20/Map/Map.java
+++
@@ -1,9 +0,0 @@
-package com.example.donpanditha.tripplannerv20.Map;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by donpanditha on 14/11/14.
- */
-public class Map extends Fragment {
-}
--- TripPlannerv2.0/app/src/main/java/com/example/donpanditha/tripplannerv20/Weather/Weather.java
+++
@@ -1,9 +0,0 @@
-package com.example.donpanditha.tripplannerv20.Weather;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by donpanditha on 14/11/14.
- */
-public class Weather extends Fragment {
-}
