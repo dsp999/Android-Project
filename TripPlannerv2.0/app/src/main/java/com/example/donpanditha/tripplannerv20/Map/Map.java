a4887de8896917916bc14218056ecf19c64247a5
dsp999 <dspanditha@yahoo.com>
2015-01-14 11:49:16 +1100

    Android app 2.0

---
+++ TripPlannerv2.0/app/src/main/java/com/example/donpanditha/tripplannerv20/Map/Map.java
@@ -0,0 +1,9 @@
+package com.example.donpanditha.tripplannerv20.Map;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by donpanditha on 14/11/14.
+ */
+public class Map extends Fragment {
+}
