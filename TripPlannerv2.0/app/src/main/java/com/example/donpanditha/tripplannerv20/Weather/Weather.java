47ae5610b5f22aceafb0744cf7ef4e31f6705f26
dsp999 <dspanditha@yahoo.com>
2015-01-14 11:53:34 +1100

    app v2.0 resubmition

---
+++ TripPlannerv2.0/app/src/main/java/com/example/donpanditha/tripplannerv20/Weather/Weather.java
@@ -0,0 +1,9 @@
+package com.example.donpanditha.tripplannerv20.Weather;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by donpanditha on 14/11/14.
+ */
+public class Weather extends Fragment {
+}
